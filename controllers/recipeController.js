a600b750f53f121d1e65985ce65f1932d7872729
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-04-28 15:37:05 +0200

    Update recipeController methods.

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,30 +1,112 @@
+import Recipe from '../models/recipe';
+import { body, validationResult } from 'express-validator';
+
 const indexRecipes = (req, res, next) => {
-  res.send('all recipes');
+  Recipe.find({})
+  .exec(function(err, recipes) {
+    if (err) { return next(err) }
+    res.send(recipes);
+    return;
+  })
 }
 
 const newRecipe = (req, res, next) => {
-  res.send('createGet');
+  res.send('new recipe')
 }
 
-const createRecipe = (req, res, next) => {
-  res.send('createPost')
-}
+const createRecipe = [
+  body('title', 'Title is required').trim().isLength({ min: 1 }),
+  body('ingredients', 'Ingredients are required.').exists(),
+  body('steps', 'Recipe steps are required.').exists(),
+  
+  body('title').escape(),
+  body('ingredients.*').escape(),
+  body('steps.*').escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const recipe = new Recipe({
+      title: req.body.title,
+      ingredients: req.body.ingredients,
+      steps: req.body.steps,
+    })
+    if (!errors.isEmpty()) {
+      res.send({ recipe: recipe, errors: errors.array() });
+      return;
+    }
+    else {
+      req.body.intro ? (recipe.intro = req.body.intro) : null;
+      req.body.image ? (recipe.image = req.body.image) : null;
+      recipe.save(function(err) {
+        if (err) { return next(err) }
+        res.send(recipe);
+      })
+    }
+  }
+];
+
 
 const editRecipe = (req, res, next) => {
-  res.send('editGet');
+  res.send('edit recipe form')
 }
 
-const updateRecipe = (req, res, next) => {
-  res.send('update recipe');
-}
+const updateRecipe = [
+  body("title", "Title is required").trim().isLength({ min: 1 }),
+  body("ingredients", "Ingredients are required.").exists(),
+  body("steps", "Recipe steps are required.").exists(),
+
+  body('title').escape(),
+  body('ingredients.*').escape(),
+  body('steps.*').escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const recipe = new Recipe({
+      title: req.body.title,
+      ingredients: req.body.ingredients,
+      steps: req.body.steps,
+      _id: req.params.id
+    });
+    req.body.intro ? (recipe.intro = req.body.intro) : null;
+    req.body.image ? (recipe.image = req.body.image) : null;
+    if (!errors.isEmpty()) {
+      res.send(errors.array());
+      return;
+    }
+    else {
+      Recipe.findByIdAndUpdate(req.params.id, recipe, {}, function (err, theRecipe) {
+        if (err) {
+          return next (err);
+        }
+        res.send(theRecipe);
+      })
+    }
+  }
+];
 
 const destroyRecipe = (req, res, next) => {
-  res.send('destroy recipe');
+  Recipe.findById(req.params.id)
+  .exec(function(err, recipe) {
+    if (err) { return next(err) }
+    Recipe.findByIdAndRemove(req.body.recipeid, function deleteRecipe(err) {
+      if (err) { return next(err) }
+      res.send('recipe deleted')
+    })
+  })
 }
 
 const showRecipe = (req, res, next) => {
-  res.send('show recipe')
-}
+  Recipe.findById(req.params.id)
+  .exec(function(err, recipe) {
+    if (err) { return next(err) }
+    if (recipe === null) {
+      const err = new Error('Recipe not found');
+      err.status = 404;
+      return next(err);
+    }
+    res.send(recipe);
+  });
+};
 
 export default {
   indexRecipes,
@@ -35,3 +117,4 @@ export default {
   destroyRecipe,
   showRecipe
 }
+
