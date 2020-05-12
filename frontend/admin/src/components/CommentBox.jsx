c9d9cad70d5cedb99fbf50c585541b6e46093485
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-05-12 13:30:15 +0200

    Add ability to delete comments from admin site.

--- frontend/admin/src/components/CommentBox.jsx
+++ frontend/admin/src/components/CommentBox.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 
 const Wrapper = styled.div`
   display: flex;
@@ -31,19 +32,62 @@ const CommentRow = styled.div`
     font-size: 0.8em;
     font-style: italic;
   }
+  .comment-delete {
+    align-self: flex-end;
+    height: 1.3em;
+    width: 1.3em;
+    border: 1px solid black;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 3px;
+    cursor: pointer;
+    &:hover {
+      color: white;
+      background: black;
+    }
+  }
 `;
 
 function CommentBox(props) {
+  const history = useHistory();
+  
+  const handleDelete = (id, recipe) => {
+    fetch(`/comments/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        commentid: id
+      })
+    })
+    .then(response => {
+      if (response.ok && response.status === 200) {
+        history.push(`/recipes/${recipe}`);
+        return response.json();
+      }
+      throw new Error('Network response was not okay.');
+    })
+    .catch(err => console.log(err.message));
+  }
+
   const generateComments = () => {
     let allComments = [];
     props.comments.map((comment, i) => {
       return allComments.push(
         <CommentRow key={i}>
+          <div 
+            className="comment-delete"
+            onClick={() => handleDelete(comment._id, comment.recipe)}
+          >x</div>
           <div className="comment-info">
             <span className="comment-name">{comment.name.toUpperCase()}</span>
             <span className="comment-date">{comment.dateFormatted}</span>
           </div>
-
           <p>{comment.content}</p>
         </CommentRow>
       )
