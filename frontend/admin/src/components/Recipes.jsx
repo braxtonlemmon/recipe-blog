16a3632edd746830e4001eb419bc399b21058aa0
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-05-12 13:12:02 +0200

    Implement ability to delete recipes on admin site.

--- frontend/admin/src/components/Recipes.jsx
+++ frontend/admin/src/components/Recipes.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Wrapper = styled.div`
   display: flex;
@@ -29,6 +29,7 @@ const Row = styled.div`
 `;
 
 function Recipes() {
+  const history = useHistory();
   const [recipes, setRecipes] = useState([]);
   const [recipesLoaded, setRecipesLoaded] = useState(false);
 
@@ -41,12 +42,26 @@ function Recipes() {
     .then(() => setRecipesLoaded(true))
   }, [])
 
-  const handleDelete = () => {
-    console.log('deleting...');
-  }
-
-  const handleEdit = () => {
-    console.log('editing...');
+  const handleDelete = (id) => {
+    fetch(`/recipes/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        recipeid: id
+      })
+    })
+    .then(response => {
+      if (response.ok && response.status === 200) {
+        history.push('/recipes');
+        return response.json();
+      }
+      throw new Error('Network response was not okay.');
+    })
+    .catch(err => console.log(err.message));
   }
 
   if (recipesLoaded) {
@@ -74,12 +89,11 @@ function Recipes() {
             </div>
             <div className="item">
               <Link to={`/recipes/${recipe._id}/edit`}>
-              <p>edit</p>
-              {/* <p className='button' onClick={handleEdit}>✎</p> */}
+                <p>edit</p>
               </Link>
             </div>
             <div className="item">
-              <p className='button' onClick={handleDelete}>🗑</p>
+              <p className='button' onClick={() => handleDelete(recipe._id)}>🗑</p>
             </div>
           </Row>
         ))}
